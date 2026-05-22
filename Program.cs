c27138dc82e82786a312926319f7be28c53451db
M1 <1366103918@qq.com>
2026-05-22 16:31:43 +0800

    更改为进度条形式

--- Program.cs
+++ Program.cs
@@ -11,18 +11,33 @@ class Program//3.类
         int remainingSeconds = totalSeconds;
 
         Console.Clear(); //清屏
-        Console.Write("🍅 PomoFocus - 专注时刻\n");
+        Console.Write("PomoFocus - 专注时刻\n");
 
-        Console.WriteLine($"番茄钟开始!专注{workMinutes} 分钟");//Console.WriteLine()能自动换行
-        Console.WriteLine($"倒计时:{remainingSeconds}秒");//Console.Write()不会自动换行
+        //Console.WriteLine($"番茄钟开始!专注{workMinutes} 分钟");//Console.WriteLine()能自动换行
+        //Console.WriteLine($"倒计时:{remainingSeconds}秒");//Console.Write()不会自动换行
         
         while(remainingSeconds > 0)
         {
-            Console.Write($"\r剩余时间:{remainingSeconds}秒");
+            //计算进度百分比 
+            int elapsed = totalSeconds - remainingSeconds;
+            int percent = elapsed * 100 / totalSeconds;
+            
+            //计算进度条长度（20格）
+            int progressLength = percent / 5;
+
+            //绘制进度条
+            string progressBar = "[" + new string('=',progressLength) + new string('-', 20-progressLength) + $"]{percent}%";
+             
+            //格式化时间显示 mm:ss
+            int minutes = remainingSeconds / 60;
+            int seconds = remainingSeconds %60;
+            string timestr = $"{minutes:D2}:{seconds:D2}";//D2代表两位数
+            Console.Write($"\r{progressBar} {timestr}");
+            //Console.Write($"\r\n剩余时间:{remainingSeconds}秒");
             Thread.Sleep(1000);//延时
             remainingSeconds--;
         }
-        Console.WriteLine("\n时间到!");
+        Console.WriteLine("\n时间到!休息一下吧！ ");
         Console.Beep(1000,1000); //发出提示音
         
         //Console.WriteLine("你好,PomoFocus");//5.输出
